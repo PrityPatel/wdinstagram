9d271aa3e8cba7cb0edc2ce3265a9d9dcb528974
Prity Patel <pritypatel714@gmail.com>
2015-08-07 16:48:24 -0700

    sourcing models in app.js file

--- app.js
+++ app.js
@@ -20,8 +20,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 //Middleware
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -29,6 +28,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+
+//Source in models
+var User = require("./models/User");
+var Entry = require("./models/Entry");
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
