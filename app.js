119ef1590d1a7b57e10caf8bbd406996163469eb
Prity Patel <pritypatel714@gmail.com>
2015-08-07 15:50:20 -0700

    updating with mongoose and router and port in app.js file and updating package json file with nodemon script and mongoose dependency

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 var express = require('express');
+var app = express();
+var mongoose = require ("mongoose");
+var http = require("http");
 var path = require('path');
 var favicon = require('serve-favicon');
+var router = express.Router();
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var port = 3000;
+
+mongoose.connect("mongodb://localhost/wdinstagram_app");
 
 var routes = require('./routes/index');
 
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//Middleware
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
