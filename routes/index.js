3cba176b7fd394e6692988b8d5285969daa4f25a
Prity Patel <pritypatel714@gmail.com>
2015-08-07 21:09:51 -0700

    added new and show route

--- routes/index.js
+++ routes/index.js
@@ -13,5 +13,20 @@ router.get('/entries', function(req, res, next){
   });
 });
 
+//NEW
+router.get('/entries/new', function(req, res, next){
+  respone.render('new', {title: 'New entry'});
+});
+
+//SHOW
+router.get('/entries/:id', function(req,res, next){
+  Entry.findOne({_id: req.params.id}, function(error,entry){
+  if (error) return res.send(error);
+  res.render('show', {title: entry.author, entry.entry_url, entry.date_taken, entry: entry });
+  });
+});
+
+
+
 
 module.exports = router;
