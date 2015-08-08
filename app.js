1bf65cd6e5f24c121a0f68355b52054ca9a6cf9b
Prity Patel <pritypatel714@gmail.com>
2015-08-07 18:25:49 -0700

    removed index route

--- app.js
+++ app.js
@@ -63,16 +63,6 @@ app.use(function(err, req, res, next) {
   });
 });
 
-//Entry Index Route
-router.get("/entries", function(request, response, next) {
-  Entry.find(function(error, entries) {
-    if (error) return response.send(error);
-    response.send(entries);
-  });
-});
-
-
-
 //Server
 var server = http.createServer(app);
 server.listen(port);
