1439ce81742c6d483fd2ccda06e968a844b53e2b
Prity Patel <pritypatel714@gmail.com>
2015-08-07 21:07:46 -0700

    added index route

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,13 @@ var entriesController = require('../controllers/entries');
 /* GET '/' - render entries index view */
 router.get('/', entriesController.renderLanding);
 
+//Index
+router.get('/entries', function(req, res, next){
+  Entry.find(function(error, entries){
+    if (error) return res.send(error);
+    res.render('index', {title: 'entries', });
+  });
+});
+
+
 module.exports = router;
