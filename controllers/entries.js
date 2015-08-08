b84e7b6b132a9a17fb680d8f9ccc4c14affbb5ec
Prity Patel <pritypatel714@gmail.com>
2015-08-07 21:50:56 -0700

    updated routes to show create before show

--- controllers/entries.js
+++ controllers/entries.js
@@ -1,30 +1,19 @@
 // Controller for Entries
 
-// GET '/' - renders entries index page
-module.exports.renderEntriesIndex = function(req, res, next) {
-  res.render('entries/index', { title: 'WDInstagram Index' });
-};
-
 // GET landing page
 module.exports.renderLanding = function(req, res, next) {
   res.render('landing_page', { title: 'WDInstagram Landing Page' });
+};
+
+// GET '/' - renders entries index page
+module.exports.renderEntriesIndex = function(req, res, next) {
+  res.render('entries/index', { title: 'WDInstagram Index' });
 };
 
 // GET 'entries/new' - renders new entry
 module.exports.renderNewEntries = function(req, res, next) {
   res.render('entries/new', { title: 'New Entry'});
 };
-
-//GET 'entries/id' - renders show one entry
-module.exports.renderShowEntry = function(req, res, next) {
-  res.render('entries/:id', {title: "An Entry"});
-};
-
-// GET 'entries/new' - renders new entry page
-module.exports.renderNewEntries = function(req, res, next) {
-  res.render('entries/new', { title: 'New Entry'});
-};
-
 // POST 'entries' - adds a new entry and redirects to index
 module.exports.postNewEntry = function(req, res, next) {
   var entry = new Entry();
@@ -35,3 +24,10 @@ module.exports.postNewEntry = function(req, res, next) {
   entry.save();
   res.redirect('/entries');
 };
+
+//GET 'entries/id' - renders show one entry
+module.exports.renderShowEntry = function(req, res, next) {
+  res.render('entries/:id', {title: "An Entry"});
+};
+
+
--- routes/index.js
+++ routes/index.js
@@ -11,26 +11,14 @@ router.get('/entries', entriesController.renderEntriesIndex);
 //NEW
 router.get('/entries/new', entriesController.renderNewEntries);
 
+// Post new entry route
+router.post('/entries', entriesController.postNewEntry);
+
 //SHOW
 router.get('/entries/:id', entriesController.renderShowEntry);
 
 
 
-//NEW
-router.get('/entries/new', function(req, res, next){
-  respone.render('new', {title: 'New entry'});
-});
-
-// Post new entry route
-router.post('/entries', entriesController.postNewEntry);
-
-//SHOW
-router.get('/entries/:id', function(req,res, next){
-  Entry.findOne({_id: req.params.id}, function(error,entry){
-  if (error) return res.send(error);
-  res.render('show', {title: entry_url});
-  });
-});
 
 
 
