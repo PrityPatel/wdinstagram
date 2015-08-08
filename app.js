6401664d034873de4935f5cbc24a3faa834b6164
Tiffany Chang <chang.tiffany@gmail.com>
2015-08-07 19:57:29 -0700

    Added method-override for put and delete requests; using bin/www for creating server

--- app.js
+++ app.js
@@ -1,29 +1,23 @@
-var express = require('express');
-var app = express();
-var mongoose = require('mongoose');
-var http = require('http');
-var path = require('path');
-var favicon = require('serve-favicon');
-var router = express.Router();
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var port = 3000;
+var express = require('express'),
+   path = require('path'),
+   favicon = require('serve-favicon'),
+   mongoose = require('mongoose'),
+   methodOverride = require('method-override'),
+   logger = require('morgan'),
+   cookieParser = require('cookie-parser'),
+   bodyParser = require('body-parser'),
+   routes = require('./routes/index'),
+   app = express();
 
-mongoose.connect('mongodb://localhost/wdinstagram_app');
-
-
-
-var routes = require('./routes/index');
-
+// Connect the DB
+mongoose.connect('mongodb://localhost/');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-//Middleware
 // uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -32,12 +26,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+ var err = new Error('Not Found');
+ err.status = 404;
+ next(err);
 });
 
 // error handlers
@@ -45,37 +38,24 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+ app.use(function(err, req, res, next) {
+   res.status(err.status || 500);
+   res.render('error', {
+     message: err.message,
+     error: err
+   });
+ });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+ res.status(err.status || 500);
+ res.render('error', {
+   message: err.message,
+   error: {}
+ });
 });
-
-// Route for showing a new entry page
-router.get('/entries/new', function(request, response, next){
-  Entry.new(function(eror) {
-    if (error) return response.send(error);
-    response.render('new', {title: 'New Entry'});
-  });
-});
-
-//Server
-var server = http.createServer(app);
-server.listen(port);
-console.log("wdinstagram_app is happening on port" + port);
 
 
 module.exports = app;
